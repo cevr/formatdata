470ec2420bbad3d0c6ade622681111a0286584e0
cev <seeve.c@gmail.com>
2018-04-30 21:41:05 -0400

    fixed

--- formatter.js
+++ formatter.js
@@ -8,8 +8,6 @@ function formatData(data) {
     let formattedData = [];
 
     for (let element of Data) {
-        let marked = false;
-
         if (isSubmodule(element.type)) {
             submodules = submodules.concat(element);
         }
@@ -17,8 +15,6 @@ function formatData(data) {
             parentModule = element;
 
             if (parentModule && submodules.length > 0) {
-                console.log('element while parentmodule exists ', element.name);
-                console.log('parentmodule:', parentModule.name);
                 formattedData = formattedData.concat({
                     ...parentModule,
                     submodules
